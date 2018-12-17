c844259288d5cedc2b63cd17f6df6d5189f5ed5d
Ariel <cestari.ariel@gmail.com>
2018-12-17 23:10:18 +0000

    Creacion de controllers

---
+++ src/controller/home.js
@@ -0,0 +1,8 @@
+const crtl = {};
+
+crtl.index = (req,res)=>{
+    res.send('Home Index page');
+};
+
+
+module.exports = crtl;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controller/home');
+const image = require('../controller/image');
+
 module.exports = app => {
-    app.get('/',(req,res)=>{
-        res.send('Index page');
-    });
+    router.get('/', home.index);
+    
+    router.get('/images/:image_id', image.index);
+    router.post('/images', image.create);
+    router.post('/images/:image_id/like', image.like);
+    router.post('/images/:image_id/image', image.coment);
+    router.delete('/images/:image_id', image.delete);
+    
+    app.use(router);
 }
