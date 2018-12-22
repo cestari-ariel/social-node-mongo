18a758ccfba4fea53faa7466ec6185bb2830622c
Ariel <cestari.ariel@gmail.com>
2018-12-22 11:00:50 +0000

    Listado de imagenes en index

--- src/controller/home.js
+++ src/controller/home.js
@@ -1,7 +1,10 @@
 const crtl = {};
+const {Image } = require('../model');
 
-crtl.index = (req,res)=>{
-    res.render('index');
+
+crtl.index = async (req,res)=>{
+    const images = await Image.find().sort({timestamp: -1});
+    res.render('index', {images: images});
 };
 
 
