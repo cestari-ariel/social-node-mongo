ce0bf108c13ce25b24b279b8b306dc449c7e4c25
Ariel <cestari.ariel@gmail.com>
2018-12-22 10:42:56 +0000

    Agrego validacion de errores al subir una img

--- src/controller/image.js
+++ src/controller/image.js
@@ -2,25 +2,53 @@ const path = require('path');
 const {randomNumber} = require('../helper/libs');
 const fs = require('fs-extra');
 
+const { Image } = require('../model/index');
+
 const ctrl = {};
 
 ctrl.index = (req, res)=>{
     
 };
 
-ctrl.create = async (req, res)=>{
-    const imgURL = randomNumber();
-    console.log(imgURL);
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const imgTempPath = req.file.path;
-    const targetPath = path.resolve(`src/public/upload/${imgURL}${ext}`);
+ctrl.create = (req, res)=>{
+    const saveImage =  async ()=>{
+        const imgURL = randomNumber();
+        const images = await Image.find({filename: imgURL});
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-        await fs.rename(imgTempPath,targetPath);
-    }
+        if(images.length > 0){
+            saveImage();
+        }else{
+            console.log(imgURL);
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const imgTempPath = req.file.path;
+            const targetPath = path.resolve(`src/public/upload/${imgURL}${ext}`);
 
-    console.log(req.file);
-    res.send("Enviado");
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                //Muevo la imagen de la carpeta temp a upload
+                await fs.rename(imgTempPath,targetPath);
+
+                //Instancio un objeto imagen
+                const newImg = new Image({
+                                    title: req.body.title,
+                                    description: req.body.description,
+                                    filename: imgURL+ext,
+                                });
+                
+                //Mando a guardar a la base de datos
+                const imageSaved = await newImg.save();
+            } else{
+                // Borro la imagen de la carpeta temp
+                await fs.unlink(imgTempPath);
+                res.status(500).json({error: 'Only images allowed'});
+            }
+           
+            res.send("Guarado");
+            //res.redirect('/images');
+                }
+            };
+
+    saveImage();    
+    
 };
 
 ctrl.like = (req, res)=>{
